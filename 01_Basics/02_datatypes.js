93be0e6454862d812e6cad384f2ce04b637a1d1d
rupsssss06 <rupakumari18105@gmail.com>
2025-02-09 15:11:14 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version
+// alert(3+3) //it will show an error if we'll try to run it coz we're using nodejs, not browser. In browser, it'll show 6 as an output
+
+console.log(3+3)
+//code readability should be high
+console.log("Rupa")
+
+let name="Rupa"
+let age=18
+let isLoggedIn=false
+
+//Primitive Datatypes:
+
+//number => 2 to power 53
+//bigint
+//string=>" "
+//boolean=>true/false
+let state=null
+//null=> standalone value(empty value)
+let state1; // undefined(Value isn't assigned yet)
+//undefined=>
+// symbol=>unqiueness
+
+
+//OBJECT:
+console.log(typeof null);// null is an object
+console.log(typeof undefined); //undefined is an undefined
+
+//IMP NOTE: undefined is a datatype while NULL is an OBJECT
+
+
