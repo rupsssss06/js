de880c2a329b9c011a9bcd7e2af315a0de8cd797
rupsssss06 <rupakumari18105@gmail.com>
2025-02-09 15:27:54 +0000

    Conversion Of Datatypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+"use strict";
+let score="true"
+console.log(typeof(score));
+
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//NOTES:
+//"33" =>33
+//"33abc"=>NaN (type of NaN(Not a Number) is also a number)
+//true=>1; false=>0
+
+
+let isLoggedIn="Rupss"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//NOTES:
+//1=>true; 0=>false
+//""=>false
+//"Rupss"=>true
+
+let someNum=343
+let stringSomeNum=String(someNum)
+console.log(typeof(stringSomeNum));
+
+console.log(stringSomeNum);
